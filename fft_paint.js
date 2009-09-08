a11f7729dc926bb90d9a231ddd02f1010deb3e8d
Patrick Stein <pat@nklein.com>
2009-09-08 16:33:32 -0500

    Added start of paint utility... need to allow color selection, brush selection, and support multiplication instead of just addition.

--- fft_paint.js
+++ fft_paint.js
@@ -7,7 +7,11 @@ var tools = new Array(
     'curve_controls'
 );
 
+var curMode = false;
 var fftData = false;
+var mouseDown = false;
+
+var modes;
 
 function selectTool( _id ) {
     var success = true;
@@ -21,6 +25,7 @@ function selectTool( _id ) {
 	    if ( tools[ii] == _id ) {
 		ctrl.style.visibility = 'visible';
 		button.src = 'true.png';
+		curMode = modes[ _id ];
 	    }
 	    else {
 		ctrl.style.visibility = 'hidden';
@@ -30,6 +35,14 @@ function selectTool( _id ) {
     }
 
     return success;
+}
+
+function __clampPixel( _rr, _ii ) {
+    var vv = Math.round( 255.0 * Math.sqrt( _rr*_rr + _ii*_ii ) );
+    if ( vv > 255 ) {
+	vv = 255;
+    }
+    return vv;
 }
 
 function __nextPowerOfTwo( _nn ) {
@@ -202,9 +215,7 @@ function __fillCanvasFromFFTData( _canvasId, _fftData ) {
 	//
 	for ( var kk=0; kk < 3; ++kk ) {
 	    var index = pp + kk;
-	    var rr = real[ index ];
-	    var ii = imag[ index ];
-	    result[ index ] = Math.round( 255.0 * Math.sqrt( rr*rr + ii*ii ) );
+	    result[ index ] = __clampPixel( real[ index ], imag[ index ] );
 	}
 	result[ pp + 3 ] = 255;
     }
@@ -241,3 +252,124 @@ function doFFT( _canvasId, _inverse ) {
     return success;
 }
 
+function mousedown() {
+    mouseDown = true;
+    return curMode && curMode.handleMouseDown && curMode.handleMouseDown();
+}
+
+function mouseup() {
+    mouseDown = false;
+    return curMode && curMode.handleMouseUp && curMode.handleMouseUp();
+}
+
+function mousemove() {
+    return curMode && ( ( mouseDown )
+			?  ( curMode.handleMouseDrag
+			     && curMode.handleMouseDrag() )
+			:  ( curMode.handleMouseTrack
+			     && curMode.handleMouseTrack() )
+	              );
+}
+
+//
+// paint controls
+//
+function __normalPaintMode( _real, _imag, _result, _offset, _r_offset,
+			    _clr_r, _clr_i, _alpha ) {
+    for ( var kk=0; kk < 3; ++kk ) {
+	var index = _offset + kk;
+	_real[index] = (1.0 - _alpha) * _real[index] + _alpha * _clr_r[kk];
+	_imag[index] = (1.0 - _alpha) * _imag[index] + _alpha * _clr_i[kk];
+
+	_result[ _r_offset + kk ] = __clampPixel( _real[index], _imag[index] );
+    }
+}
+
+var paint_color_r = Array( 1.0, 0.0, 0.0 );
+var paint_color_i = Array( 0.0, 0.0, 0.0 );
+var paint_opacity = 0.2;
+var paint_mode = __normalPaintMode;
+var brush = Array(
+  Array( 0.1, 0.2, 0.5, 0.2, 0.1 ),
+  Array( 0.2, 0.7, 0.9, 0.7, 0.2 ),
+  Array( 0.5, 0.9, 1.0, 0.9, 0.5 ),
+  Array( 0.2, 0.7, 0.9, 0.7, 0.2 ),
+  Array( 0.1, 0.2, 0.5, 0.2, 0.1 )
+);
+
+function paintAtMouse() {
+    var canvas = document.getElementById( 'the_canvas' );
+    if ( !canvas ) {
+	return false;
+    }
+
+    if ( !fftData ) {
+	fftData = __createFFTDataFromCanvas( canvas.id );
+	if ( !fftData ) {
+	    return false;
+	}
+    }
+
+    canvas = document.getElementById( 'the_canvas' );
+    if ( !canvas ) {
+	return false;
+    }
+
+    var context = canvas.getContext('2d');
+    if ( !context ) {
+	return false;
+    }
+
+    var bh = brush.length;
+    var bw = brush[0].length;
+    var mx = event.clientX - canvas.offsetLeft;
+    var my = event.clientY - canvas.offsetTop;
+
+    var bound_xx = Math.max( mx - (bw-1)/2, 0 );
+    var bound_ww = Math.min( mx + (bw-1)/2, canvas.width ) - bound_xx;
+    var bound_yy = Math.max( my - (bh-1)/2, 0 );
+    var bound_hh = Math.min( my + (bh-1)/2, canvas.height ) - bound_yy;
+
+    if ( bound_ww < 1 || bound_hh < 1 ) {
+	return false;
+    }
+    var rawResult = context.getImageData(bound_xx,bound_yy,bound_ww,bound_hh);
+    var result = rawResult.data;
+
+    var real = fftData.real;
+    var imag = fftData.imag;
+
+    for ( var bj = 0, r_index = 0; bj < bh; ++bj ) {
+	var yy = my + bj - (bh-1)/2;
+	if ( 0 <= yy && yy < canvas.height ) {
+	    var yoff = yy * canvas.width * 4;
+
+	    for ( var bi = 0; bi < bw; ++bi ) {
+		var xx = mx + bi - (bw-1)/2;
+		if ( 0 <= xx && xx < canvas.width ) {
+		    var index = yoff + xx * 4;
+		    window.status = "(" + xx + "," + yy + ") = " + index;
+		    paint_mode( real, imag, result, index, r_index,
+				paint_color_r, paint_color_i,
+				paint_opacity );
+		    r_index += 4;
+		}
+	    }
+	}
+    }
+
+    context.putImageData( rawResult, bound_xx, bound_yy );
+}
+
+//
+// assign all of the mode handlers
+//
+function logMouseMove() {
+    window.status = "Mouse move: ("+ event.clientX + "," + event.clientY +")";
+}
+modes = {
+    paint_controls: {
+	handleMouseDrag: paintAtMouse,
+	handleMouseTrack: logMouseMove,
+    },
+}
